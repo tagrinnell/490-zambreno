0d2056487881d628654d62a8a02dc29f7ad6056e
tagrinnell <tasman.grinnell@gmail.com>
2024-09-07 15:06:36 -0500

    Wrote more of the matrix multiplication alg

--- matrixMulSharedMem/matMulNormal/matMul.cpp
+++ matrixMulSharedMem/matMulNormal/matMul.cpp
@@ -6,18 +6,30 @@
 **/
 
 #include <cstdlib>
+#include <iostream>
+#include <string>
 
 // number of rows for the matrix
-constexpr int rows = 15;
+constexpr int rows = 3;
 
 void randMatVals(int** matrix);
+void matMul(int** matA, int** matB, int** outputMat);
+void printMat(int** matrix, std::string string);
 
 int main() {
 
-    int** mat = (int**) malloc(sizeof(int) * rows * rows);
+    int** matA = (int**) malloc(sizeof(int) * rows * rows);
+    int** matB = (int**) malloc(sizeof(int) * rows * rows);
+    int** outMat = (int**) malloc(sizeof(int) * rows * rows);
 
-    randMatVals(mat);
+    randMatVals(matA);
+    randMatVals(matB);
 
+    matMul(matA, matB, outMat);
+
+    printMat(matA, "Matrix A");
+    printMat(matB, "Matrix B");
+    printMat(outMat, "Output Matrix");
 }
 
 void randMatVals(int** matrix) {
@@ -27,4 +39,35 @@ void randMatVals(int** matrix) {
             matrix[i][j] = rand() % 200;
         }
     }
+}
+
+void matMul(int** matA, int** matB, int** outputMat) {
+
+    int outMatX = 0;
+    int outMatY = 0;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < rows; j++) {
+            for (int x = 0; x < rows; x++) {
+                outputMat[i][j] += matA[i][x] * matB[x][j];
+            }
+        }
+    }
+
+}
+
+void printMat(int** mat, std::string string) {
+
+    std::cout << string << std::endl;
+    
+    for (int i = 0; i < rows; i++) {
+        std::cout << "[ ";
+
+        for (int j = 0; j < rows; j++) {
+            std::cout << mat[i][j] << "\t";            
+        }
+
+        std::cout << " ]" << std::endl;
+    }
+
 }
