4e77ea7d8c0edc3acfc0e64c6599677c37a2d251
Tasman Grinnell <tgrin@iastate.edu>
2024-09-14 10:53:54 -0500

    I think there's something wronng with my kernel calculation, trying to fix it

--- edgeDetection/edgeDetectionSequential/canny_sequential.cpp
+++ edgeDetection/edgeDetectionSequential/canny_sequential.cpp
@@ -23,11 +23,9 @@
 constexpr int kernelSize = 5;
 
 void cannyEdgeDetection (int filterSize);
-void grayscale (int width, int height, int channels);
+void grayscale(int width, int height, int channels, std::vector<char> v);
 void gaussianKernel (double kernel[kernelSize][kernelSize]);
-void gaussianBlur(int width, int height);
-
-unsigned char* image;
+void gaussianBlur(int width, int height, std::vector<char> imV);
 
 int main () {
     std::cout << "Beginning run" << std::endl;
@@ -40,16 +38,18 @@ void cannyEdgeDetection (int filterSize) {
     // Steps for Algorithm
     // open image
     int width, height, channels;
+    unsigned char *image;
+    
     image = stbi_load("..\\numbat.jpeg", &width, &height, &channels, 3);
     std::vector<char> iVector(image, image + width * height * channels);
 
     std::cout << "Loaded image with a width of " << width << ", a height of " << height << " and " << channels << " channels" << std::endl; 
 
     // Grayscale the Image to 
-    grayscale(width, height, channels);
+    grayscale(width, height, channels, iVector);
     
     // Noise reduction (Gaussian blur/filter)
-    gaussianBlur(width,height);
+    gaussianBlur(width,height, iVector);
 
     // Gradient Calculation
     // Non-maximum suppression
@@ -65,37 +65,21 @@ void cannyEdgeDetection (int filterSize) {
  * https://tannerhelland.com/2011/10/01/grayscale-image-algorithm-vb6.html
  * 
  */
-void grayscale(int width, int height, int channels) {
 
-    // Allocate
-    unsigned char* grayImage = (unsigned char*) malloc(width * height * sizeof(unsigned char));
-
-    // Gray = (Red * 0.2126 + Green * 0.7152 + Blue * 0.0722)
-    for (int imageCtr = 0; imageCtr < width * height * channels; imageCtr += channels) {
-        grayImage[imageCtr / 3] = (image[imageCtr] * 0.2126 + image[imageCtr + 1] * 0.7152 + image[imageCtr + 2] * 0.0722);
-    }
-
-    memcpy(image, grayImage, width * height);
-
-    // Write grayscaled Image as a log
-    stbi_write_jpg("..\\exampleImages\\grayscaled.jpg", width, height, 1, image, width);
-
-}
-
-void grayscale(int width, int height, int channels, std::vector<char> vector) {
+void grayscale(int width, int height, int channels, std::vector<char> v) {
 
     // Allocate
     std::vector<char> grayVec;
 
     // Gray = (Red * 0.2126 + Green * 0.7152 + Blue * 0.0722)
     for (int imageCtr = 0; imageCtr < width * height * channels; imageCtr += channels) {
-        grayVec.push_back(); // image[imageCtr] * 0.2126 + image[imageCtr + 1] * 0.7152 + image[imageCtr + 2] * 0.0722);
+        grayVec.push_back(v.at(imageCtr) * 0.2126 + v.at(imageCtr + 1) * 0.7152 + v.at(imageCtr + 2) * 0.0722); // image[imageCtr] * 0.2126 + image[imageCtr + 1] * 0.7152 + image[imageCtr + 2] * 0.0722);
     }
 
-    memcpy(image, grayVec, width * height);
+    v = grayVec;
 
     // Write grayscaled Image as a log
-    stbi_write_jpg("..\\exampleImages\\grayscaled.jpg", width, height, 1, image, width);
+    stbi_write_jpg("..\\outputImages\\grayscaled.jpg", width, height, 1, reinterpret_cast<char*>(grayVec.data()), width);
 
 }
 
@@ -108,7 +92,7 @@ void gaussianKernel (double kernel[kernelSize][kernelSize]) {
  
     for (int i = 0; i < kernelSize; i++) {
         for (int j = 0; j < kernelSize; j++) {
-            kernel[i][j] = exp( -0.5 * (pow((i - mean) / sigma, 2.0) + pow((j - mean) / sigma, 2.0)) ) / (2 * M_PI * sigma * sigma);
+            kernel[i][j] = exp(-1.0 * ((i * i + j * j) / (2 * sigma * sigma))) / (2 * M_PI * sigma * sigma);
             sum += kernel[i][j];
         }
     }
@@ -143,41 +127,39 @@ for each image row in input image:
 
          set output image pixel to accumulator
 */
-void gaussianBlur(int width, int height) {
+void gaussianBlur(int width, int height, std::vector<char> imV) {
     double kernel[kernelSize][kernelSize];
 
     gaussianKernel(kernel);
-    int len = sizeof(image) / sizeof(*image);
-
-    stbi_write_jpg("..\\exampleImages\\preblur.jpg", width, height, 1, image, width);
+    int len = imV.size();
 
     // Convolve the matrix
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            float accumX = 0;
-            float accumY = 0;
+    // for (int i = 0; i < height; i++) {
+    //     for (int j = 0; j < width; j++) {
+    //         float accumX = 0;
+    //         float accumY = 0;
             
-            for (int k = 0; k < kernelSize; k++) {
-                for (int l = 0; l < kernelSize; l++) {
-                    float valX = 0.0;
-                    float valY = 0.0;
+    //         for (int k = 0; k < kernelSize; k++) {
+    //             for (int l = 0; l < kernelSize; l++) {
+    //                 float valX = 0.0;
+    //                 float valY = 0.0;
 
-                    // If we're reaching into the next row (overflowing on right side of image), set multiplier to 0.0
-                    if ((i + k) * width + j + l > width * (i + k + 1) || (i + k) * width + j + l > width * height) {
-                        valX = 0.0;
-                        valY = 0.0;
-                    } else {
-                        valX = image[(i + k) * width];
-                        valY = image[(i + k) * width + j + l];
-                    }
+    //                 // If we're reaching into the next row (overflowing on right side of image), set multiplier to 0.0
+    //                 if ((i + k) * width + j + l > width * (i + k + 1) || (i + k) * width + j + l > width * height) {
+    //                     valX = 0.0;
+    //                     valY = 0.0;
+    //                 } else {
+    //                     valX = image[(i + k) * width];
+    //                     valY = image[(i + k) * width + j + l];
+    //                 }
 
-                    accumX += valX * kernel[k][l];
-                    accumY += valY * kernel[k][l];
-                }
-            }
-            image[i * width + j] = (char) sqrt(accumX * accumX + accumY * accumY); // fmax(0, fmin(255, 
-        }
-    }
+    //                 accumX += valX * kernel[k][l];
+    //                 accumY += valY * kernel[k][l];
+    //             }
+    //         }
+    //         image[i * width + j] = (char) sqrt(accumX * accumX + accumY * accumY); // fmax(0, fmin(255, 
+    //     }
+    // }
 
-    stbi_write_jpg("..\\exampleImages\\gaussBlur.jpg", width, height, 1, image, width);
+    // stbi_write_jpg("..\\exampleImages\\gaussBlur.jpg", width, height, 1, image, width);
 }
